d86d261e026888497771e8909c15d0c8f522c9a7
Brian <thisbrian@pm.me>
2025-07-27 02:56:10 +1200

    refactor: restructure workspace namings and configure for ICP deployment

--- front/landing/astro.config.mjs
+++ front/landing/astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: 'static',
+  server: {
+    port: 5173
+  },
   redirects: {
     "/discord": "https://discord.gg/Ane3ppsEGU",
     "/twitter": "https://x.com/tryposted",
